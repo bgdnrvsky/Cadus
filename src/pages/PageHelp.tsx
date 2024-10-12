fa04b5d0136ca07ed5d533b4a45c9e52545485e8
Jean-BaptisteRamette <jeanbaptisterametteperso@gmail.com>
2024-10-12 16:42:45 +0200

    Implement Carousel component

--- src/pages/PageHelp.tsx
+++ src/pages/PageHelp.tsx
@@ -1,6 +1,17 @@
+import Carousel from "../components/Carousel";
 
 export default function PageHelp() {
+
+    const images = [
+        'https://via.placeholder.com/2000x400/ff5733/fff',
+        'https://via.placeholder.com/2000x400/33ff57/fff',
+        'https://via.placeholder.com/2000x400/5733ff/fff',
+    ];
+
     return (
-        <h1>Nous aider</h1>
+        <>
+            <Carousel images={images} infinite={true}></Carousel>
+            <h1>Nous aider</h1>
+        </>
     );
 }
