af9b3e77143adbe697fe354862447c9bf3f5742c
Jean-BaptisteRamette <jeanbaptisterametteperso@gmail.com>
2024-10-12 19:52:16 +0200

    fix default colors not working because of config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,6 +8,19 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      stone: colors.warmGray,
+      sky: colors.lightBlue,
+      neutral: colors.trueGray,
+      gray: colors.coolGray,
+      slate: colors.blueGray,
+
       'cadus-black': '#1E1E1E',
       'cadus-grey': '#757575',
       'cadus-green': '#94BFA7',
