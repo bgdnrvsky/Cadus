4f4a516d58c01abe4ff7b951625090f282bb473c
Jean-BaptisteRamette <jeanbaptisterametteperso@gmail.com>
2024-10-17 20:57:43 +0200

    decrease padding in Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,13 +10,15 @@ export default function Card(props: CardProps) {
     const { quote } = props;
 
     return (
-        <figure className="h-fit rounded-[30px] border bg-cadus-card shadow-md p-5">
+        <figure className="h-fit rounded-[30px] border bg-cadus-card shadow-md p-4">
             <blockquote className="text-center text-2xl sm:text-3xl font-semibold">
                 <p>"{quote}"</p>
             </blockquote>
 
-            <figcaption className="flex items-center space-x-4 mt-6">
-                <img className="flex-none size-10 rounded-full object-cover" src={require('../assets/profile_pictures/avatar1.jpg')} alt=""/>
+            <figcaption className="flex items-center space-x-4 mt-4">
+                <img className="flex-none size-10 rounded-full object-cover"
+                     src={require('../assets/profile_pictures/avatar1.jpg')} alt=""/>
+
                 <div className="flex-auto">
                     <p className="text-slate-600 font-semibold">Huguette - 74 ans</p>
                     <p className="text-slate-400">Bénéficiaire</p>
