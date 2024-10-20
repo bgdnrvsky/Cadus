873621c3524e3f59f1e027c76a7656983362c648
bgdnrvsky <raevskijb@gmail.com>
2024-10-20 22:07:36 +0200

    Trying to make it responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,18 +12,38 @@ function NavLinkItem({ path, name }: {path: string; name: string } ) {
 }
 
 export default function NavBar() {
-    return (
-        <nav className="bg-cadus-green p-4">
-            <div className="flex flex-wrap justify-between max-sm:justify-center">
-                <img src={cadusLogo} className="rounded-full h-14 bg-white" alt="Logo Cadus"/>
+    const navLinkItems = (
+      <>
+          <NavLinkItem path="/" name="L'association"/>
+          <NavLinkItem path="/nousaider" name="Nous aider"/>
+          <NavLinkItem path="/livredor" name="Livre d'or"/>
+          <NavLinkItem path="/remerciements" name="Remerciements"/>
+      </>
+    );
 
-                <div className="flex">
-                    <NavLinkItem path="/" name="L'association"/>
-                    <NavLinkItem path="/nousaider" name="Nous aider"/>
-                    <NavLinkItem path="/livredor" name="Livre d'or"/>
-                    <NavLinkItem path="/remerciements" name="Remerciements"/>
-                </div>
-            </div>
-        </nav>
+    const svgColor = "white"
+
+    return (
+<nav className="bg-cadus-green p-4">
+    <div className="flex justify-between">
+        <img src={cadusLogo} className="rounded-full h-14 bg-white" alt="Logo Cadus"/>
+
+        <ul className="flex max-sm:hidden">
+            {
+                navLinkItems
+            }
+        </ul>
+
+        <svg className="sm:max-2xl:hidden" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g stroke-width="0"></g>
+            <g stroke-linecap="round" stroke-linejoin="round"></g>
+            <g>
+                <path d="M4 18L20 18" stroke={svgColor} stroke-width="2" stroke-linecap="round"></path>
+                <path d="M4 12L20 12" stroke={svgColor} stroke-width="2" stroke-linecap="round"></path>
+                <path d="M4 6L20 6" stroke={svgColor} stroke-width="2" stroke-linecap="round"></path>
+            </g>
+        </svg>
+    </div>
+</nav>
     );
 }
