a1f65feb88ad889c90a9b525649e453b0ab2fc56
Jean-BaptisteRamette <jeanbaptisterametteperso@gmail.com>
2024-11-02 15:54:36 +0100

    add shadow to scroll indicator

--- src/components/ScrollIndicator.tsx
+++ src/components/ScrollIndicator.tsx
@@ -9,7 +9,7 @@ export default function ScrollIndicator(props: ScrollIndicatorProps) {
 
     return (
         <div className="flex justify-center mb-6">
-            <a className="animate-bounce border-2 border-transparent text-white rounded-full hover:border-cadus-green hover:text-cadus-green transition" href={targetId}>
+            <a className="animate-bounce shadow-lg border-2 border-transparent text-white rounded-full hover:border-cadus-green hover:text-cadus-green transition" href={targetId}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-gray-800"
                      fill="none"
                      viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
