d9b2f2db48d2d5230e071e4d3207bae389840b6f
bgdnrvsky <raevskijb@gmail.com>
2024-11-04 19:01:16 +0100

    Add margins around cards' div

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -8,7 +8,7 @@ export default function Book() {
                 <p className="mt-4 text-center text-cadus-black font-bold text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">Livre d'or</p>
             </div>
 
-            <div className="mx-4 mt-1.5 grid grid-flow-dense grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 items-center">
+            <div className="mx-8 mb-10 mt-10 grid grid-flow-dense grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 items-center">
                 <Card quote="Mme H., je tiens à vous remercier du fond du coeur de votre soutien, de votre patience, de votre persévérance et de votre écoute durant ces longues années de combat."/>
                 <Card quote="Je tiens à vous remercier très chaleureusement pour tous les conseils que vous m’avez apportés depuis le début de cette affaire et je ne manquerai pas de faire à nouveau appel à votre association en cas de besoin"/>
                 <Card quote="L’Association et vous même vous avez su nous entendre, et nous aider à faire reconnaître l’erreur médicale c’est pour la mémoire de mon papa que nous avons fait appel à vous"/>
