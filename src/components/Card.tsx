b472f3b334a1126c595f8a79431ec63d71498c64
bgdnrvsky <raevskijb@gmail.com>
2024-11-07 22:28:24 +0100

    More arguments for the `Card`

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,12 +5,18 @@ interface CardProps {
 
 		/* Filepath to the image */
 		path: string;
+
+		/* Paragraph text */
+		text: string;
+
+		/* Underparagraph text */
+		subtext: string;
 }
 
 
 export default function Card(props: CardProps) {
 
-    const { quote, path } = props;
+    const { quote, path, text, subtext } = props;
 
     return (
         <figure className="h-fit rounded-[30px] border bg-cadus-card shadow-md p-5">
@@ -21,8 +27,8 @@ export default function Card(props: CardProps) {
             <figcaption className="flex items-center space-x-4 mt-6">
                 <img className="flex-none size-10 rounded-full object-cover" src={path} alt=""/>
                 <div className="flex-auto">
-                    <p className="text-slate-600 font-semibold">Huguette - 74 ans</p>
-                    <p className="text-slate-400">Bénéficiaire</p>
+                    <p className="text-slate-600 font-semibold">{text}</p>
+                    <p className="text-slate-400">{subtext}</p>
                 </div>
             </figcaption>
         </figure>
