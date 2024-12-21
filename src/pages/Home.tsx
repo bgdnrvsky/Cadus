d931a93dc846f9750368c0c71c7fbfb3b4615aa6
Jean-BaptisteRamette <jeanbaptisterametteperso@gmail.com>
2024-12-21 12:53:36 +0100

    cleanup Home page a bit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import cadusLogo from "../assets/cadus.svg";
 import img1 from "../assets/home/img1.png";
 import img2 from "../assets/home/img2.jpg";
 import img3 from "../assets/home/img3.jpeg";
-import { Link } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import LinkButton from "../components/LinkButton";
 
@@ -12,7 +11,7 @@ export default function Home() {
     "Conseiller, aider et défendre des usagers de la santé sur le plan juridique et judiciaire.",
     "Aide à obtenir votre dossier médical.",
     "Aide à la constitution de votre demande d’indemnisation",
-    "Conseils tout au long de votre parcours jusqu’à l’indemnisation.",
+    "Conseille tout au long de votre parcours jusqu’à l’indemnisation.",
     "Participer à l’information des victimes des accidents médicaux et de leurs proches, et leur apporter de l'aide",
     "Participer directement ou indirectement à la défense judiciaire des patients infectés victimes d’un accident médical, d’une infection nosocomiale.",
     "Assurer la défense et les intérêts des usagers du système de santé d’une affection iatrogène ou d’un aléa thérapeutique.",
@@ -32,17 +31,13 @@ export default function Home() {
     <div>
         <NavBar/>
       <div className="flex items-center justify-around p-1">
-        <img
-          src={cadusLogo}
-          className="h-64 w-64 bg-white home-logo"
-          alt="Logo Cadus"
-        />
+        <img src={cadusLogo} className="h-96 w-72 bg-white" alt="Logo Cadus"/>
         <div className="text-center w-1/2 ">
           <h3 className="text-2xl font-bold">
-            Vous etes victime d'erreurs médicales ?
+            Vous êtes victime d'erreurs médicales ?
           </h3>
           <p className="text-lg mt-2">
-            L'assistance de la C.A.D.U.S conseil les victime dans les actions
+            Cadus conseille les victimes dans les actions
             contentieuses pour l'obtention de l'indemnisation des dommages
             subis.
           </p>
@@ -55,9 +50,9 @@ export default function Home() {
       </div>
 
       <div className="flex justify-around mt-24">
-        <img src={img1} className="w-1/3 home-img" alt="Image 1" />
-        <img src={img3} className="w-1/3 home-img" alt="Image 3" />
-        <img src={img2} className="w-1/3 home-img" alt="Image 2" />
+        <img src={img1} className="w-1/3 h-[450px]" alt="Image 1"/>
+        <img src={img3} className="w-1/3 h-[450px]" alt="Image 3"/>
+        <img src={img2} className="w-1/3 h-[450px]" alt="Image 2"/>
       </div>
 
       <div className="text-center p-8">
@@ -74,13 +69,14 @@ export default function Home() {
         <h2 className="text-2xl font-bold mb-4">L'aide apportée</h2>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 items-center mt-8 p-8">
-        {aideList.map((item, index) => (
-          <div
-            key={index}
-            className="cases w-96 h-32 bg-white shadow-md rounded-lg flex text-center items-center p-6"
-          >
-            <p>{item}</p>
-          </div>
+        {
+            aideList.map((item, index) => (
+                <div
+                  key={index}
+                  className="m-4 bg-white shadow-md rounded-lg flex text-center items-center p-6 hover:scale-105 hover:shadow-md hover:shadow-[#80008033]"
+                >
+                  <p>{item}</p>
+                </div>
         ))}
       </div>
       <Footer />
