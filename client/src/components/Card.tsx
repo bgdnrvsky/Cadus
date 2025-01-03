5ad762c51a1e6dd16def645c59b49a3f8f374e72
bgdnrvsky <raevskijb@gmail.com>
2025-01-03 15:55:29 +0100

    client: Make `Card` accept children

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,8 +1,6 @@
+import React from "react";
 
-interface CardProps {
-    /* Quote displayed in the card */
-    quote: string;
-
+interface CardAvatarProps {
     /* Path to quoted person's avatar */
     avatar: string;
 
@@ -13,24 +11,44 @@ interface CardProps {
     status: string;
 }
 
+export function CardAvatar(props: CardAvatarProps) {
+    const {avatar, name, status} = props;
 
-export default function Card(props: CardProps) {
+    return (
+        <figcaption className="flex items-center space-x-4 mt-6">
+            <img className="flex-none size-10 rounded-full object-cover" src={avatar} alt=""/>
+            <div className="flex-auto">
+                <p className="text-slate-600 font-semibold">{name}</p>
+                <p className="text-slate-400">{status}</p>
+            </div>
+        </figcaption>
+    );
+}
 
-    const { quote, avatar, name, status } = props;
+interface CardQuoteProps {
+    children: React.ReactNode;
+}
+
+export function CardQuote(props: CardQuoteProps) {
+    const {children} = props;
+
+    return (
+        <blockquote className="text-center text-base md:text-3xl font-semibold">
+            <p>{children}</p>
+        </blockquote>
+    );
+}
+
+interface CardProps {
+    children: React.ReactNode;
+}
+
+export function Card(props: CardProps) {
+    const {children} = props;
 
     return (
         <figure className="h-fit rounded-[30px] border bg-cadus-card shadow-md p-5">
-            <blockquote className="text-center text-base md:text-3xl font-semibold">
-                <p>«{quote}»</p>
-            </blockquote>
-
-            <figcaption className="flex items-center space-x-4 mt-6">
-                <img className="flex-none size-10 rounded-full object-cover" src={avatar} alt=""/>
-                <div className="flex-auto">
-                    <p className="text-slate-600 font-semibold">{name}</p>
-                    <p className="text-slate-400">{status}</p>
-                </div>
-            </figcaption>
+            {children}
         </figure>
     );
 }
--- client/src/pages/Help.tsx
+++ client/src/pages/Help.tsx
@@ -1,5 +1,5 @@
 import Carousel, {CarouselSlide} from "../components/Carousel";
-import Card from "../components/Card";
+import {Card, CardAvatar, CardQuote} from "../components/Card";
 import Button from "../components/Button";
 import ScrollIndicator from "../components/ScrollIndicator";
 import NavBar from "../components/NavBar";
@@ -17,7 +17,8 @@ export default function Help() {
                     <CarouselSlide image={require("../assets/volunteer.jpg")}>
                         <div className="flex flex-col items-center md:items-start space-y-6">
                             <h1 className="text-6xl text-white text-center font-bold">Rejoindre Cadus.</h1>
-                            <p className="text-lg text-white text-center md:text-left w-2/3">Acme Outdoors is an outdoor and adventure shop located in the Boathouse District in Oklahoma City.</p>
+                            <p className="text-lg text-white text-center md:text-left w-2/3">Acme Outdoors is an outdoor
+                                and adventure shop located in the Boathouse District in Oklahoma City.</p>
                             <a href="#volunteer-section"><Button className="text-white">En savoir plus</Button></a>
                         </div>
                     </CarouselSlide>
@@ -25,7 +26,8 @@ export default function Help() {
                     <CarouselSlide image={require("../assets/promote.jpg")}>
                         <div className="flex flex-col items-center md:items-start space-y-6">
                             <h1 className="text-6xl text-white text-center font-bold">Promouvoir Cadus.</h1>
-                            <p className="text-lg text-white text-center md:text-left w-2/3">Acme Outdoors is an outdoor and adventure shop located in the Boathouse District in Oklahoma City.</p>
+                            <p className="text-lg text-white text-center md:text-left w-2/3">Acme Outdoors is an outdoor
+                                and adventure shop located in the Boathouse District in Oklahoma City.</p>
                             <a href="#promote-section"><Button className="text-white">Comment faire ?</Button></a>
                         </div>
                     </CarouselSlide>
@@ -33,7 +35,8 @@ export default function Help() {
                     <CarouselSlide image={require("../assets/donate.jpg")}>
                         <div className="flex flex-col items-center md:items-start space-y-6">
                             <h1 className="text-6xl text-white text-center font-bold">Soutenir Cadus.</h1>
-                            <p className="text-lg text-white text-center md:text-left w-2/3">Acme Outdoors is an outdoor and adventure shop
+                            <p className="text-lg text-white text-center md:text-left w-2/3">Acme Outdoors is an outdoor
+                                and adventure shop
                                 located in the Boathouse District in Oklahoma City.</p>
                             <a href="#donate-section"><Button className="text-white">J'apporte mon soutien</Button></a>
                         </div>
@@ -54,18 +57,24 @@ export default function Help() {
                         </div>
 
                         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 m-10">
-                            <Card avatar={require("../assets/profile_pictures/avatar9.jpg")}
-                                  name="Marie"
-                                  status="Bénéficiaire"
-                                  quote="Je ne me sentais plus seule et sans défense grâce à Cadus"/>
-                            <Card avatar={require("../assets/profile_pictures/avatar10.jpg")}
-                                  name="Sylvain"
-                                  status="Bénévole"
-                                  quote="Soutenir un bénéficiaire était une expérience très enrichissante"/>
-                            <Card avatar={require("../assets/profile_pictures/avatar8.jpg")}
-                                  name="Joe"
-                                  status="Bénéficiaire"
-                                  quote="Je n'aurais pas réussi à obtenir justice sans Cadus"/>
+                            <Card>
+                                <CardQuote>Je ne me sentais plus seule et sans défense grâce à Cadus</CardQuote>
+                                <CardAvatar avatar={require("../assets/profile_pictures/avatar9.jpg")}
+                                            name="Marie"
+                                            status="Bénéficiaire"/>
+                            </Card>
+                            <Card>
+                                <CardQuote>Soutenir un bénéficiaire était une expérience très enrichissante</CardQuote>
+                                <CardAvatar avatar={require("../assets/profile_pictures/avatar10.jpg")}
+                                            name="Sylvain"
+                                            status="Bénévole"/>
+                            </Card>
+                            <Card>
+                                <CardQuote>Je n'aurais pas réussi à obtenir justice sans Cadus"</CardQuote>
+                                <CardAvatar avatar={require("../assets/profile_pictures/avatar8.jpg")}
+                                            name="Joe"
+                                            status="Bénéficiaire"/>
+                            </Card>
                         </div>
                         <Button className="m-auto block text-cadus-green">Demander une formation</Button>
                     </div>
@@ -78,16 +87,19 @@ export default function Help() {
                 <div className="flex flex-col md:flex-row flex-grow items-center">
                     <div className="font-display w-1/2">
                         <p className="text-center md:ml-32 mb-6 mt-4 md:inline-block font-bold text-cadus-black text-5xl brush-underline">Promouvoir</p>
-                        <p className="hidden md:block ml-32 pt-4 text-left text-wrap text-cadus-grey text-1xl">Êtes-vous artiste,
+                        <p className="hidden md:block ml-32 pt-4 text-left text-wrap text-cadus-grey text-1xl">Êtes-vous
+                            artiste,
                             designer ou imprimeur ? Souhaitez-vous mettre votre talent au service d’une cause qui a du
                             sens ? Nous vous invitons à rejoindre Cadus, une initiative qui soutient les personnes
                             confrontées à des injustices médicales.</p>
-                        <p className="hidden md:block ml-32 pt-4 text-left text-wrap text-cadus-grey text-1xl">Votre créativité et votre
+                        <p className="hidden md:block ml-32 pt-4 text-left text-wrap text-cadus-grey text-1xl">Votre
+                            créativité et votre
                             expertise pourraient faire une réelle différence. Que vous souhaitiez collaborer directement
                             ou que vous ayez des idées pour promouvoir notre mission, chaque contribution compte. Que ce
                             soit par le biais de vos réseaux, de vos créations ou d'autres initiatives, votre aide
                             serait précieuse.</p>
-                        <p className="md:ml-32 pt-4 text-center md:text-left text-wrap text-cadus-grey text-1xl">Si vous êtes intéressé(e)
+                        <p className="md:ml-32 pt-4 text-center md:text-left text-wrap text-cadus-grey text-1xl">Si vous
+                            êtes intéressé(e)
                             à offrir votre soutien pour la promotion de Cadus d’une manière ou d’une autre, n’hésitez
                             pas à nous contacter dès maintenant. Ensemble, nous pouvons créer un impact positif et faire
                             avancer cette cause importante !
@@ -95,7 +107,8 @@ export default function Help() {
                     </div>
 
                     <div className="relative w-full mt-40 md:mt-0 md:flex-1 flex justify-center">
-                        <div className="bg-cadus-green rounded-l-xl absolute inset-x-0 top-1/2 transform -translate-y-1/2 translate-x-16 rotate-3 w-[101%] h-[120%] -z-10"></div>
+                        <div
+                            className="bg-cadus-green rounded-l-xl absolute inset-x-0 top-1/2 transform -translate-y-1/2 translate-x-16 rotate-3 w-[101%] h-[120%] -z-10"></div>
                         <div className="w-3/4 md:w-1/2">
                             <PromoteForm/>
                         </div>
@@ -109,23 +122,27 @@ export default function Help() {
                 <div className="flex flex-col md:flex-row-reverse items-center">
                     <div className="font-display w-1/2">
                         <p className="text-center md:mr-32 md:inline-block mb-6 mt-4 font-bold text-cadus-black text-5xl brush-underline">Donner</p>
-                        <p className="text-center md:mr-32 pt-4 md:text-left text-wrap text-cadus-grey text-1xl">Chez Cadus, nous nous
+                        <p className="text-center md:mr-32 pt-4 md:text-left text-wrap text-cadus-grey text-1xl">Chez
+                            Cadus, nous nous
                             engageons à soutenir les victimes d'injustice médicale, qui souffrent souvent en silence.
                             Grâce à votre don, nous pouvons offrir des consultations juridiques, un soutien
                             psychologique et des campagnes de sensibilisation pour faire entendre leur voix. Chaque euro
                             compte et peut transformer des vies.</p>
-                        <p className="hidden md:block mr-32 pt-4 text-left text-wrap text-cadus-grey text-1xl">Votre contribution permet
+                        <p className="hidden md:block mr-32 pt-4 text-left text-wrap text-cadus-grey text-1xl">Votre
+                            contribution permet
                             également de plaider pour des changements essentiels dans le système de santé. En nous
                             aidant, vous participez à la défense des droits des victimes et à l’amélioration des
                             pratiques médicales. Ensemble, nous pouvons apporter une aide concrète à ceux qui en ont
                             besoin.</p>
-                        <p className="hidden md:block mr-32 pt-4 text-left text-wrap text-cadus-grey text-1xl">Faites un don aujourd'hui
+                        <p className="hidden md:block mr-32 pt-4 text-left text-wrap text-cadus-grey text-1xl">Faites un
+                            don aujourd'hui
                             pour faire la différence. Votre générosité montre que chaque vie mérite d’être protégée et
                             respectée. Merci de votre soutien dans cette lutte pour la justice et l’espoir.</p>
                     </div>
 
                     <div className="relative w-full mt-40 md:mt-0 md:flex-1 flex justify-center">
-                        <div className="bg-cadus-green rounded-r-xl absolute -rotate-3 inset-x-0 top-1/2 transform -translate-y-1/2 -translate-x-16 w-[101%] h-[120%] -z-10"></div>
+                        <div
+                            className="bg-cadus-green rounded-r-xl absolute -rotate-3 inset-x-0 top-1/2 transform -translate-y-1/2 -translate-x-16 w-[101%] h-[120%] -z-10"></div>
                         <DonationForm/>
                     </div>
                 </div>
