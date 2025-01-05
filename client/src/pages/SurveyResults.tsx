b1ead2f338d2faeb9055cf2e6dee563b271b522d
Jean-BaptisteRamette <jeanbaptisterametteperso@gmail.com>
2025-01-05 19:27:59 +0100

    Ajout du dernier graphique

--- client/src/pages/SurveyResults.tsx
+++ client/src/pages/SurveyResults.tsx
@@ -1,6 +1,6 @@
 import GraphicMap from "../components/d3/GraphicMap";
 import GraphicAnswersCount from "../components/d3/GraphicAnswersCount";
-import NavBar from "../components/NavBar";
+import GraphicPieChart from "../components/d3/GraphicPieChart";
 
 
 export default function SurveyResults() {
@@ -8,14 +8,20 @@ export default function SurveyResults() {
         <div className="h-screen overflow-y-scroll overflow-x-hidden snap-y snap-mandatory scroll-smooth">
             <div className="snap-start h-full w-full flex flex-col">
                 <div className="flex-1 flex flex-col items-center justify-center">
-                    <h2 className="text-3xl">I - Répartition des personnes interrogées dans la France métropolitaine</h2>
+                    <h2 className="text-3xl">1. Répartition des personnes interrogées dans la France
+                        métropolitaine</h2>
                     <GraphicMap/>
                 </div>
             </div>
 
             <div className="snap-start h-screen w-full flex flex-col items-center justify-center space-y-10">
-                <h2 className="text-3xl">II - Répartition des réponses par question</h2>
+                <h2 className="text-3xl">2. Répartition des réponses par question</h2>
                 <GraphicAnswersCount/>
+            </div>
+
+            <div className="snap-start h-screen w-full flex flex-col items-center justify-center space-y-10">
+                <h2 className="text-3xl">3. Répartition des réponses pour les aides</h2>
+                <GraphicPieChart/>
             </div>
         </div>
     );
