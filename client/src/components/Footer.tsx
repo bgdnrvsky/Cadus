78514e6a162502e355c63581c522e08483edf8d6
bgdnrvsky <raevskijb@gmail.com>
2025-01-06 16:42:48 +0100

    Improve footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,26 +3,30 @@ import mail from "../assets/home/mail.png";
 import localisation from "../assets/home/localisation.png";
 
 export default function Footer() {
+    const mailAdress = "Association@cadus.fr";
+
     return (
-        <footer className="p-8 text-center">
-            <h2 className="text-3xl font-bold mb-4">Nous contacter</h2>
+        <footer className="p-8 bg-cadus-green">
+            <div className="flex flex-row flex-wrap">
+                <h2 className="text-3xl font-bold mb-4">Nous contacter</h2>
 
-            <div className="flex flex-col items-center space-y-4">
-                <div className="flex items-center space-x-2">
-                    <img src={mail} alt="Icone mail" className="h-6 w-6"/>
-                    <p className="text-lg">Association@cadus.fr</p>
-                </div>
+                <div className="flex flex-row flex-wrap mt-2">
+                    <div className="flex ml-5">
+                        <img src={mail} alt="Icone mail" className="h-6 w-6 mr-2"/>
+                        <a href={"mailto:" + mailAdress} className="text-lg">{mailAdress}</a>
+                    </div>
 
-                <div className="flex items-center space-x-2">
-                    <img src={localisation} alt="Icone localisation" className="h-6 w-6"/>
-                    <p className="text-lg whitespace-nowrap">
-                        8 Rue Jean GIONO, 49100 ANGERS
-                    </p>
+                    <div className="flex ml-5">
+                        <img src={localisation} alt="Icone localisation" className="h-6 w-6 mr-2"/>
+                        <p className="text-lg whitespace-nowrap">
+                            8 Rue Jean GIONO, 49100 ANGERS
+                        </p>
+                    </div>
                 </div>
             </div>
 
-            <p className="mt-8 text-sm">
-                Pour nous contacter, adressez-nous un message, nous vous fixerons un RDV téléphonique. Merci.
+            <p className="text-sm max-sm:mt-3">
+                Adressez-nous un message et nous vous fixerons un RDV téléphonique, merci
             </p>
         </footer>
     );
